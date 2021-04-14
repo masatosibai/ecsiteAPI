d6398e9c20dab9ee31634a29f230d485ce588ff9
masato <maboshiva@gmail.com>
2021-04-14 15:00:29 +0900

    test

---
+++ tests/Feature/APIAdminTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class APIAdminTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        //権限がちがうからエラー
+        $response = $this->post('/api/admin/register/shopadmin', [
+            'name' => 'admin',
+            'email' => 'sample@d.com',
+            'password' => '11111',
+            'admin' => '2',
+            'role' => '2',
+
+        ]);
+
+        $response->assertStatus(404);
+
+        $response = $this->post('/api/admin/register/shopadmin', [
+            'name' => 'admin',
+            'email' => 'sample@d.com',
+            'password' => '11111',
+            'admin' => '1',
+            'role' => '2',
+
+        ]);
+
+        $response->assertStatus(200);
+    }
+}
